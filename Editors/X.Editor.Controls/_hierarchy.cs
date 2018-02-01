fd79f08cc2ce0957ff15235c4b594c57b1549c8b
Brezho <glebars@jadelogistics.com>
2018-02-01 10:43:09 +0100

    hmmm

---
+++ Editors/X.Editor.Controls/_hierarchy.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using X.Editor.Controls.Adornment;
+using X.Editor.Controls.Controls;
+using X.Editor.Controls.Utils;
+using X.Editor.Model;
+
+namespace X.Editor.Controls
+{
+    public class TestHierarchyProvider : IHierarchyProvider
+    {
+        public Hierarchy CreateHierarchy(IEditorShell shell)
+        {
+            return new TestHierarchy(shell);
+        }
+    }
+    public class TestHierarchy : Hierarchy
+    {
+        IEditorShell shell;
+
+        public TestHierarchy(IEditorShell shell)
+        {
+            this.shell = shell;
+            BindRoot();
+        }
+
+        void BindRoot()
+        {
+            var terminalsFolder = this.AddFolder("Test");
+            terminalsFolder.RegisterEditorBuilder(typeof(Control), () => new MySurface() { Dock = DockStyle.Fill });
+        }
+    }
+
+    public class MySurface : Surface, IEditor
+    {
+        IEditorContainer container;
+
+        void Adorn(Control ctrl)
+        {
+            Adorn<Positioner>(ctrl);
+            Adorn<Resizer>(ctrl);
+            var connector = Adorn<Connector>(ctrl);
+            connector.Add("titi", new Point(12, 12));
+        }
+
+        public void ActivateIn(IEditorContainer newWindow)
+        {
+            container = newWindow;
+
+            var oscillo = new Oscilloscope();
+            oscillo.Location = new Point(400, 400);
+            this.Controls.Add(oscillo);
+            Adorn(oscillo);
+
+            var t = new TestGraph();
+            t.Location = new Point(200, 200);
+            this.Controls.Add(t);
+            Adorn(t);
+        }
+    }
+}
